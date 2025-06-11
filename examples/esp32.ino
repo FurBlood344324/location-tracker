dfde3e5e052d342225666e39f71e19eb209c9274
metharda <ahmetardaorak@gmail.com>
2025-06-12 01:17:11 +0300

    fixed esp32 example

--- examples/esp32.ino
+++ examples/esp32.ino
@@ -3,25 +3,18 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-// === GPS setup ===
-// ESP32 UART2 pinleri (HardwareSerial)
 #define GPS_RX_PIN 16
 #define GPS_TX_PIN 17
 HardwareSerial &neogps = Serial2;
 TinyGPSPlus gps;
 double lat = 0.0, lng = 0.0;
 
-// === Wi-Fi & HTTP setup ===
-// Kendi ağ bilgilerinizi girin
 const char* ssid       = "your-ssid";
 const char* password   = "your-password";
-// POST endpoint
-const char* serverURL  = "https://map.arda.engineer/location";
-// POST aralığı (ms)
+const char* serverURL  = "your-server-url";
 const unsigned long postInterval = 60UL * 1000;
 unsigned long lastPostTime = 0;
 
-// --- Yardımcı fonksiyonlar ---
 String charToString(char c) {
   char tmp[2] = { c, '\0' };
   return String(tmp);
@@ -57,7 +50,7 @@ void updateLatLngIfMatch(const String &line) {
   }
   if (idx < 7 || parts[2] != "A") return;
 
-  // Enlem ddmm.mmmm, N/S
+
   double rawLat = parts[3].toDouble();
   char ns = parts[4].charAt(0);
   double dLat = floor(rawLat / 100.0);
@@ -65,7 +58,7 @@ void updateLatLngIfMatch(const String &line) {
   lat = dLat + mLat / 60.0;
   if (ns == 'S') lat = -lat;
 
-  // Boylam dddmm.mmmm, E/W
+
   double rawLng = parts[5].toDouble();
   char ew = parts[6].charAt(0);
   double dLng = floor(rawLng / 100.0);
@@ -97,10 +90,10 @@ void sendLocation() {
 void setup() {
   Serial.begin(115200);
 
-  // GPS UART2 başlat (9600, 8N1) RX2=16, TX2=17
+
   neogps.begin(9600, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
 
-  // Wi-Fi bağlantısı
+
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(100);
@@ -114,7 +107,7 @@ void setup() {
 }
 
 void loop() {
-  // GPS'ten gelen NMEA satırlarını oku ve TinyGPS++'a ver
+
   while (neogps.available()) {
     String nmea = readLineFromStream(neogps);
     Serial.println(nmea);
@@ -122,17 +115,17 @@ void loop() {
     for (char c : nmea) gps.encode(c);
   }
 
-  // TinyGPS++ güncellemesi
+
   if (gps.location.isUpdated()) {
     lat = gps.location.lat();
     lng = gps.location.lng();
     Serial.println("Koordinat güncellendi!");
   }
 
-  // Seri port çıktısı
+
   printLatLng(lat, lng);
 
-  // Periyotla POST at
+
   if (millis() - lastPostTime >= postInterval) {
     sendLocation();
     lastPostTime = millis();
